f30b0e3c7d291301d24e2c882a1d115a249e97b9
Kruthi Nymisha <kruthinymisha@gmail.com>
2020-12-12 01:54:56 -0500

    Updates

---
+++ precache-manifest.0e21224bc522be02ee4bbe72d721dfc5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229540cbb907e2f69308640886210aad",
+    "url": "/seainfo6150-final-project-webapp/index.html"
+  },
+  {
+    "revision": "2687ea7ce0e6de97c05d",
+    "url": "/seainfo6150-final-project-webapp/static/css/main.0331a273.chunk.css"
+  },
+  {
+    "revision": "5ea6fda97ec17ee9149f",
+    "url": "/seainfo6150-final-project-webapp/static/js/2.0d28caa0.chunk.js"
+  },
+  {
+    "revision": "2687ea7ce0e6de97c05d",
+    "url": "/seainfo6150-final-project-webapp/static/js/main.d8e10907.chunk.js"
+  },
+  {
+    "revision": "940ec4a42937c37ae625",
+    "url": "/seainfo6150-final-project-webapp/static/js/runtime~main.c4d7f5e8.js"
+  },
+  {
+    "revision": "78f0060bb188b37017f70b1863785675",
+    "url": "/seainfo6150-final-project-webapp/static/media/Birthday.78f0060b.jpg"
+  },
+  {
+    "revision": "47e46c29e19f847cf4f0dcc896505150",
+    "url": "/seainfo6150-final-project-webapp/static/media/Cupcakes.47e46c29.jpg"
+  },
+  {
+    "revision": "c9b1978501090d951cb50a8a16f72acb",
+    "url": "/seainfo6150-final-project-webapp/static/media/Errors.c9b19785.gif"
+  },
+  {
+    "revision": "639940fe7080ecda28c24f3268016dbc",
+    "url": "/seainfo6150-final-project-webapp/static/media/Thanks.639940fe.gif"
+  },
+  {
+    "revision": "6bb9de831fa2c394d7bb50b2756664e5",
+    "url": "/seainfo6150-final-project-webapp/static/media/Weddinghome.6bb9de83.jpg"
+  },
+  {
+    "revision": "96d71995de4e873944db9fea155e52e1",
+    "url": "/seainfo6150-final-project-webapp/static/media/aboutUs.96d71995.jpg"
+  },
+  {
+    "revision": "11a23252c0c6279e4245950207e0e985",
+    "url": "/seainfo6150-final-project-webapp/static/media/cakeshome.11a23252.jpg"
+  },
+  {
+    "revision": "3500f8a6e54f0a1f31748f673eded7c8",
+    "url": "/seainfo6150-final-project-webapp/static/media/cookies.3500f8a6.jpg"
+  },
+  {
+    "revision": "6f45a4bdac2cb8ec6b3c7d658ad306e9",
+    "url": "/seainfo6150-final-project-webapp/static/media/logo.6f45a4bd.JPG"
+  }
+]);
